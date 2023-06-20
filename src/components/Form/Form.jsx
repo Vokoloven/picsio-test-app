ad84fe972fc1fbfb0bcce6b3e3152854fc8b9795
Ruslan Volovenko <vokoloven@gmail.com>
2023-06-21 01:20:39 +0300

    Fix for form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -10,6 +10,7 @@ export const Form = ({ localValue, onChangeHandler, onSubmitHandler }) => {
       width={"800px"}
       position={"relative"}
       onSubmit={onSubmitHandler}
+      id={"form"}
     >
       <Textarea value={localValue} onChange={onChangeHandler} />
       <SubmitButton type="submit" aria-label={"Submit Button"}>
