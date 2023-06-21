97b63b3ee0ca9b81a9aa6c6197aac67536be2433
Ruslan Volovenko <vokoloven@gmail.com>
2023-06-21 15:26:05 +0300

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@ import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/picsio-test-app/",
   resolve: {
